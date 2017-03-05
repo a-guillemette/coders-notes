997223d865c83e4cd432f4749407aba6a09907f9
Zihark17 <yannick0395@hotmail.fr>
2017-03-05 15:20:36 -0500

    Create core and api projects

---
+++ api/src/api.ts
@@ -0,0 +1,18 @@
+import { createServer, Server, ServerOptions } from "restify";
+
+const port = 8426;
+const serverOptions: ServerOptions = {
+
+};
+
+const server: Server = createServer(serverOptions);
+
+server.get('hello/:name', (req, res, next) => {
+    res.send('Hello ' + req.params.name);
+    next();
+});
+
+server.listen(port, () => {
+    console.log('\r\n');
+    console.log('%s listening at %s', server.name, server.url);
+});
