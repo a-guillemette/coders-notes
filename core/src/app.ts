8d54057518e410e03622169b81005fa9852e4497
Antoine Guillemette <antoine.guillemette@hotmail.com>
2017-03-15 21:21:28 -0400

    export statements update

--- core/src/app.ts
+++ core/src/app.ts
@@ -1,1 +1,5 @@
 export { User } from './objects/user';
+export { Language, LanguageId } from './objects/language';
+export { Snippet, SnippetId } from './objects/snippet';
+export { Label, LabelId } from './objects/label';
+export { Image, ImageId } from './objects/image';
