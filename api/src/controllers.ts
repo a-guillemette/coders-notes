d9e2ad66ddad8f3256895d1b4314104b59403c13
Antoine Guillemette <antoine.guillemette@hotmail.com>
2017-08-23 23:53:30 -0400

    Added Status Controller with hard coded statuses

--- api/src/controllers.ts
+++ api/src/controllers.ts
@@ -1,7 +1,9 @@
 import { AuthenticationController } from './controllers/authentication.controller';
 import { TestController } from './controllers/test.controller';
+import {StatusController} from './controllers/status.controller';
 
 export const Controllers: Array<any> = [
     AuthenticationController,
-    TestController
+    TestController,
+    StatusController
 ];
