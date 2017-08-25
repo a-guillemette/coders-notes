7d3358b4541aa28c90ad1ff2c9c9c053756f1089
Yannick <yannick0395@hotmail.fr>
2017-08-24 22:10:44 -0400

    Improve batch file to run db scripts

--- mongodb/scripts/000 - run_scripts.bat
+++ mongodb/scripts/000 - run_scripts.bat
@@ -6,6 +6,12 @@ SET user="admin"
 SET pwd="Password1"
 SET /P nrun=Start scripts from number (###):
 
+IF "!nrun!" == "" (
+	echo No number provided
+	pause
+	exit
+)
+
 for %%a in (*.js) do (
     set filename=%%a
     set number=!filename:~0,3!
@@ -13,6 +19,8 @@ for %%a in (*.js) do (
     IF /I !number! GEQ !nrun! (
         echo Run script #!number!
         mongo codersnotes --username "!user!" --password "!pwd!" "%%a"
+		echo.
+		echo.
     )
 )
 pause
