db2962089319b62bad310a8f8a6b8eb81ec8140f
Antoine Guillemette <antoine.guillemette3868@gmail.com>
2017-09-05 22:19:29 -0400

    Modified Label class to implement BaseDataObject and label controller to use it.

--- api/src/controllers/label.controller.ts
+++ api/src/controllers/label.controller.ts
@@ -1,31 +1,71 @@
-import {HttpDelete, HttpPost, Route, RoutePrefix} from '../decorators/route.decorator';
+import {HttpDelete, HttpPost, HttpPut, Route, RoutePrefix} from '../decorators/route.decorator';
 import {Request, Response, Next} from 'restify';
 import {HttpGet} from '../decorators/route.decorator';
+import {DatabaseService} from '../services/database.service';
+import {Label, DataObject} from '@codersnotes/core';
 
 
 @RoutePrefix('label')
 export class LabelController {
-    @HttpPost @Route('')
-    getLabels(req: Request, res: Response, next: Next) {
-        res.send(200, 'want some fuk');
-        next();
-    }
+	@HttpGet @Route('')
+	getLabels(req: Request, res: Response, next: Next) {
+		DatabaseService.db.collection(Label.name).find<Label>().toArray((error, labels) => {
+			if (error) {
+				res.send(500);
+			} else {
+				res.send(labels);
+			}
+			next();
+		});
+	}
 
-    @HttpGet @Route(':id')
-    getLanguage(req: Request, res: Response, next: Next) {
-        res.send(200, 'want some fuk');
-        next();
-    }
+	@HttpGet @Route('/:id')
+	getLabel(req: Request, res: Response, next: Next) {
+		DatabaseService.db.collection(Label.name).findOne<Label>({_id: req.params.id}, (error, label) => {
+			if (error) {
+				res.send(500);
+			} else {
+				res.send(label);
+			}
+			next();
+		});
+	}
 
-    @HttpPost @Route(':id')
-    editLanguage(req: Request, res: Response, next: Next) {
-        res.send(200, 'want some fuk');
-        next();
-    }
+	@HttpPost @Route('')
+	createLabel(req: Request, res: Response, next: Next) {
+		const label = DataObject.from(Label, req.body);
+		DatabaseService.db.collection(Label.name).insertOne(label, (error, result) => {
+			if (error) {
+				res.send(500);
+			} else {
+				res.send(200, label);
+			}
+			next();
+		});
+	}
 
-    @HttpDelete @Route(':id')
-    removeLabel(req: Request, res: Response, next: Next) {
-        res.send(200, 'want som fk');
-        next();
-    }
+	@HttpPut @Route('/:id')
+	updateLabel(req: Request, res: Response, next: Next) {
+		const label = DataObject.from(Label, req.body);
+		DatabaseService.db.collection(Label.name).updateOne({_id: label._id}, label, (error, result) => {
+			if (error) {
+				res.send(500);
+			} else {
+				res.send(200, label);
+			}
+			next();
+		});
+	}
+
+	@HttpDelete @Route('/:id')
+	removeLabel(req: Request, res: Response, next: Next) {
+		DatabaseService.db.collection(Label.name).deleteOne({_id: req.params.id}, (error, result) => {
+			if (error) {
+				res.send(500);
+			} else {
+				res.send(200, result);
+			}
+			next();
+		});
+	}
 }
--- core/src/objects/label.ts
+++ core/src/objects/label.ts
@@ -1,6 +1,8 @@
+import { Prop, BaseDataObject } from '../data-object';
+
 export type LabelId = number;
 
-export class Label {
-    _id: LabelId;
-    text: string;
+export class Label extends BaseDataObject {
+    @Prop() _id: LabelId;
+    @Prop() text: string;
 }
